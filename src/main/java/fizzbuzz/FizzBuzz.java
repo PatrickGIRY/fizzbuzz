ea00dd62e6f2ac2b9fb064705583d8607001fb1a
Patrick GIRY <patrickgiry@gmail.com>
2016-08-23 00:22:00 +0200

    refactor(fizzbuzz): replace if by rules.

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -1,17 +1,40 @@
 package fizzbuzz;
 
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import static fizzbuzz.FizzBuzz.Rule.rule;
+import static java.util.stream.Collectors.joining;
+
 public class FizzBuzz {
     public static String fizzbuzz(int value) {
-        if (value > 0 && value % 15 == 0) {
-            return "FizzBuzz";
+
+        RuleChecker ruleChecker = RuleChecker.fromValue(value);
+        return mayBe(value, Stream.of(rule(3, "Fizz"), rule(5, "Buzz")).filter(ruleChecker).map(rule -> rule.s).collect(joining()));
+
+    }
+
+    private static String mayBe(int value, String s) {
+        return s.isEmpty() ? String.valueOf(value) : s;
+    }
+
+    static class Rule {
+        private final int multipleOf;
+        private final String s;
+
+        public static Rule rule(int multipleOf, String s) {
+            return new Rule(multipleOf, s);
         }
-        else if (value > 0 && value % 5 == 0) {
-           return "Buzz";
+
+        private Rule(int multipleOf, String s) {
+            this.multipleOf = multipleOf;
+            this.s = s;
         }
-        else if (value > 0 && value % 3 == 0) {
-            return "Fizz";
-        } else {
-            return String.valueOf(value);
+    }
+
+    private interface RuleChecker extends Predicate<Rule> {
+        static RuleChecker fromValue(int value) {
+            return rule -> value > 0 && value % rule.multipleOf == 0;
         }
     }
 }
